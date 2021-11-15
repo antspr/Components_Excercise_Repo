1d7ce82c2686ce8daa9bbfd3f03d9951a02fbe33
antspr <antoinesprings@outlook.com>
2021-11-15 13:07:21 -0500

    initial setup for Worksheet

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import reactDom from 'react-dom';
+import App from './components/App'
+
+const jsxElement = <h1>Components Exercise Lab</h1>
+
+reactDom.render(jsxElement, document.getElementById('root'));
